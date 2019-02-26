1cf17d7493e2104a8ebf86d1095ab6a43157770e
Ashton Bailey <ashtonkbailey@gmail.com>
2019-02-25 19:30:35 -0700

    Change HelloWorld to Photos list and update App to bring Photos in

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">FotoVuer</h1>
+    <h3 class="search-word">results for ''</h3>
+    <Photos />
+    <form>
+      <input />
+      <button>search</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Photos from './components/Photos.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Photos
   }
 }
 </script>
---
+++ src/components/Photos.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="photo-list">
+    <li class="photo"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Photos',
+  data: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
